0eeb4a524caa179d6c2fc9b4358b2e46afe70550
enhbayasgalan <enhuush0109@gmail.com>
2024-12-02 19:11:31 +0800

    parta

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("heloo world");
+
+const name = "bataa"
+
+console.log("bataa");
+
+const firstname = "baysaa"
+
+console.log("baysaa");
+
+const myname = "orgil"
+console.log("orfil")
+
+const ovog = "ogii";
+const ner = "altaa";
+const nas = "12";
+console.log(ovog, ner, nas,)
